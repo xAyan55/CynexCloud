ac48eb3e75ad1e5aa76fe08ba0a430d4d837be41
xAyan55 <arvindica@gmail.com>
2026-07-01 10:56:47 +0530

    Whitelist all domains with custom wildcard CORS middleware

--- server.ts
+++ server.ts
@@ -114,6 +114,16 @@ const FieldValue = {
 async function startServer() {
   const app = express();
   app.use(express.json());
+  
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
   const httpServer = createServer(app);
   const io = new Server(httpServer, {
     cors: {
