3e77b3ba144d7cc281690a3ad8c0543186231a32
xAyan55 <arvindica@gmail.com>
2026-07-02 16:37:28 +0530

    fix: wrap app with AppProviders (AuthProvider + QueryClient)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import AppProviders from '@/app/providers'
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </StrictMode>,
 );
